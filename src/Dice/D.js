a9001ed47eb6ce4a3d9bcdd44d5ea6f10c735b91
D-BS21WG <yves.peeters2@ing.be>
2016-06-09 20:35:49 +0200

    updated test to support a fromString method on the dice generator

--- src/Dice/D.js
+++ src/Dice/D.js
@@ -1,5 +1,5 @@
-module.exports = (function(){
-    var die = function(value){
+module.exports = (function () {
+    var die = function (value) {
         return (Math.random() * (value - 1) + 1);
     };
     return {
@@ -23,9 +23,36 @@ module.exports = (function(){
                 "hundred": die(100) + die(100)
             }
         },
-        "four":{
-            "D":{
+        "four": {
+            "D": {
                 "six": die(6) + die(6) + die(6) + die(6)
+            }
+        },
+        "fromString": function (str) {
+            var numbers =
+                {
+                    "1" :"one",
+                    "2" :"two" ,
+                    "3" :"three",
+                    "4" : "four" ,
+                    "6" : "six",
+                    "8" : "eight" ,
+                    "10" : "ten" ,
+                    "20" : "twenty" ,
+                    "100" : "hundred"
+                }
+                ;
+            var arr = str.split('D'),
+             part1 = arr[0],
+            part2 = arr[1];
+
+            if (arr.length == 2 && isFinite(part1) && isFinite(part2)){
+                console.log(numbers[arr[0]]);
+                console.log(numbers[arr[1]]);
+                return numbers[arr[0]] + ".D." + numbers[arr[1]];
+            }
+            else{
+                throw "fromString recieved an invalid string. expl: fromString('1D6').";
             }
         }
     }
